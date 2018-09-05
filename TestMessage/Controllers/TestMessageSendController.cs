4499900da1a696393841ed51202754b33f980b1c
Vezir Mir <vezirmir@gmail.com>
2018-09-05 12:12:09 +0300

    Add project files.

---
+++ TestMessage/Controllers/TestMessageSendController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TestMessage.Models;
+
+namespace TestMessage.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestMessageSendController : Controller
+    {
+        // GET api/values
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return null; //new string[] { "value1", "value2" };
+        //}
+
+        // GET api/values/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]MessageModel value)
+        {
+            string json = JsonConvert.SerializeObject(value);
+            StringContent stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.PostAsync("https://api.pushover.net/1/messages.json", stringContent);
+            return StatusCode(Convert.ToInt32(response.StatusCode));
+        }
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ TestMessage/Models/MessageModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMessage.Models
+{
+    public class MessageModel
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+        [JsonProperty("user")]
+        public string User { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
